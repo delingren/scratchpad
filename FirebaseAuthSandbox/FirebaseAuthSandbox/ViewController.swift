5cb46ba4e9553de45260c7fe724c5dc5f0398931
Deling Ren <delingren@Delings-Hack.local>
2018-12-19 08:14:09 -0800

    Facebook login

--- FirebaseAuthSandbox/FirebaseAuthSandbox/ViewController.swift
+++ FirebaseAuthSandbox/FirebaseAuthSandbox/ViewController.swift
@@ -14,7 +14,10 @@ class ViewController: UIViewController, FUIAuthDelegate {
 
     var auth: Auth?
     var authUI: FUIAuth?
-    let providers: [FUIAuthProvider] = [FUIGoogleAuth(),]
+    let providers: [FUIAuthProvider] = [
+        FUIGoogleAuth(),
+        FUIFacebookAuth(),
+        ]
     
     var handle: AuthStateDidChangeListenerHandle?
     
@@ -41,10 +44,40 @@ class ViewController: UIViewController, FUIAuthDelegate {
         auth!.removeStateDidChangeListener(handle!)
     }
     
+    @IBAction func loginPressed(_ sender: Any) {
+    }
+    
     @IBAction func loginAction(sender: AnyObject) {
         // Present the default login view controller provided by authUI
         let authViewController = authUI?.authViewController();
         self.present(authViewController!, animated: true, completion: nil)
+    }
+    
+    @IBAction func logoutPressed(_ sender: Any) {
+        do {
+            try authUI!.signOut()
+        } catch {
+            print("Failed to sign out")
+        }
+    }
+    
+    func authUI(_ authUI: FUIAuth, didSignInWith user: User?, error: Error?) {
+        guard let authError = error else {
+            print("Signed in: \(user!)")
+            return
+        }
+        
+        let errorCode = UInt((authError as NSError).code)
+        
+        switch errorCode {
+        case FUIAuthErrorCode.userCancelledSignIn.rawValue:
+            print("User cancelled sign-in");
+            break
+            
+        default:
+            let detailedError = (authError as NSError).userInfo[NSUnderlyingErrorKey] ?? authError
+            print("Login error: \((detailedError as! NSError).localizedDescription)");
+        }
     }
 }
 
