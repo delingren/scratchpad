8d9abcc7f499f451eafb08bbd1f8b32df025f748
Deling Ren <delingren@Delings-MacBook-Pro.local>
2018-12-29 18:15:26 -0800

    Message prompt and spinner ported to swift

---
+++ FirebaseAuthSandbox/FirebaseAuthSandbox/UIViewControllerExt.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewControllerExt.swift
+//  FirebaseAuthSandbox
+//
+//  Created by Deling Ren on 12/29/18.
+//  Copyright © 2018 Deling Ren. All rights reserved.
+//
+
+import Foundation
