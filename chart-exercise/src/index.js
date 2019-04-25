c75299ce04d9f2db9c7bad41ed296f3d2147f6ad
delingren <cookiejar.seattle@gmail.com>
2019-04-24 23:14:46 -0700

    Yet another chart

--- chart-exercise/src/index.js
+++ chart-exercise/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import ChartReactChartJS from './App1';
 import ChartCanvasJS from './App2';
+import FancyChart from './App3';
 
 //ReactDOM.render(<ChartReactChartJS />, document.getElementById('root'));
-ReactDOM.render(<ChartCanvasJS />, document.getElementById('root'));
+//ReactDOM.render(<ChartCanvasJS />, document.getElementById('root'));
+ReactDOM.render(<FancyChart />, document.getElementById('root'));
